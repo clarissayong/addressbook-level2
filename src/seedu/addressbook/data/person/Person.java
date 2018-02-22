3358c1fe38230b85ea18743dfd61bcdbc03f282d
Clarissa Yong <35588995+clarissayong@users.noreply.github.com>
2018-02-22 13:33:18 +0800

    Update Person.java

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -8,7 +8,7 @@ import java.util.*;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson{
+public class Person implements ReadOnlyPerson {
 
     private Name name;
     private Phone phone;
